7450850dde1fe20a7721b9f1fb3ab9e5bbfc4ec0
paenis <46578974+paenis@users.noreply.github.com>
2023-05-13 19:58:04 -0400

    unset idempotent so that git sha actually works

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     EmitBuilder::builder()
-        .idempotent()
         .git_sha(true)
         .git_branch()
         .cargo_opt_level()
