d53da1ee226344cd8bfca54d77120b47cb690095
paenis <46578974+paenis@users.noreply.github.com>
2023-05-13 19:31:02 -0400

    git sha in version number, bump crate, change name

---
+++ build.rs
@@ -0,0 +1,11 @@
+use vergen::EmitBuilder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    EmitBuilder::builder()
+        .idempotent()
+        .git_sha(true)
+        .git_branch()
+        .cargo_opt_level()
+        .emit()?;
+    Ok(())
+}
