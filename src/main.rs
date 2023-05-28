2ee5715aa3d1795dccd4ffd4afaa9e82cd65b2a5
paenis <46578974+paenis@users.noreply.github.com>
2023-05-28 19:39:59 -0400

    builder -> derive

--- src/main.rs
+++ src/main.rs
@@ -8,171 +8,220 @@ pub(crate) mod common;
 pub(crate) mod types;
 pub(crate) mod utils;
 
+use async_once::AsyncOnce;
 use clap::error::ErrorKind;
-use clap::{arg, command, value_parser, ArgAction, ArgGroup, Command};
-use color_eyre::eyre::{self, eyre, Result, WrapErr};
+use clap::{arg, command, value_parser, Args, CommandFactory, Parser, Subcommand, ValueEnum};
+use color_eyre::eyre::{eyre, Result, WrapErr};
 use is_terminal::IsTerminal;
 use itertools::Itertools;
+use lazy_static::lazy_static;
+use prettytable::format::FormatBuilder;
+use prettytable::{row, Cell, Row, Table};
 
 use crate::common::MCDL_VERSION;
-use crate::types::version::{GameVersion, VersionNumber};
+use crate::types::version::{GameVersionList, VersionNumber};
 use crate::utils::net::get_version_manifest;
+
+lazy_static! {
+    static ref MANIFEST: AsyncOnce<GameVersionList> = AsyncOnce::new(async {
+        get_version_manifest()
+            .await
+            .expect("Failed to get version manifest")
+    });
+}
+
+#[doc(hidden)]
+#[derive(Parser, Debug)]
+#[command(author, version = MCDL_VERSION.as_str())]
+#[command(arg_required_else_help = true, subcommand_required = true)]
+/// A tool for managing Minecraft server versions
+struct Cli {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[doc(hidden)]
+#[derive(Subcommand, Debug)]
+enum Action {
+    /// List available Minecraft versions
+    List {
+        #[command(flatten)]
+        filter: Option<ListFilter>,
+        // #[arg(short, long)]
+        // #[arg(rename = "kebab-case")]
+        // /// List installed instances and their versions
+        // installed: bool,
+    },
+    /// Get information about a Minecraft version
+    Info {
+        #[arg(required = true, value_parser = value_parser!(VersionNumber))]
+        #[arg(short, long)]
+        /// The Minecraft version to get information about
+        version: VersionNumber,
+    },
+    /// Install a server instance
+    Install {
+        #[arg(value_delimiter = ',', num_args = 0.., value_parser = value_parser!(VersionNumber))]
+        #[arg(short, long)]
+        /// The version(s) to install
+        ///
+        /// Defaults to latest release version if none is provided.
+        /// Can be specified multiple times, or as a comma or space-separated list.
+        version: Option<Vec<VersionNumber>>,
+        // #[arg(short, long)]
+        // name: Option<String>,
+    },
+    /// Run a server instance
+    Run {
+        #[arg(short, long)]
+        version: String, // in the future, `name` will be used instead
+    },
+    /// Print the path to a config file or instance directory
+    Locate {
+        #[arg(required = true)]
+        #[arg(value_enum)]
+        /// The file or directory to locate
+        what: WhatEnum,
+    },
+    // /// Uninstall a server instance
+    // Uninstall {
+    //     #[arg(short, long)]
+    //     version: String, // in the future, `name` will be used instead
+    // },
+}
+
+#[doc(hidden)]
+#[derive(Args, Debug)]
+#[group(id = "filter", required = false, multiple = false)]
+struct ListFilter {
+    #[arg(short, long)]
+    /// Only list release versions (default)
+    release: bool,
+    #[arg(short, long)]
+    /// Only list pre-release versions
+    pre_release: bool,
+    #[arg(short, long)]
+    /// Only list snapshot versions
+    snapshot: bool,
+    #[arg(short, long)]
+    /// Only list other versions
+    other: bool,
+    #[arg(short, long)]
+    /// List all versions
+    all: bool,
+}
+
+impl Default for ListFilter {
+    fn default() -> Self {
+        Self {
+            release: true,
+            pre_release: false,
+            snapshot: false,
+            other: false,
+            all: false,
+        }
+    }
+}
+
+#[doc(hidden)]
+#[derive(Clone, Copy, ValueEnum, Debug)]
+enum WhatEnum {
+    /// The Java Runtime Environment directory
+    Java,
+    /// The directory containing Minecraft server instances
+    Instance,
+    /// The directory containing configuration files
+    Config,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let mut cmd = command!()
-        .about("A tool for managing Minecraft server versions")
-        .version(MCDL_VERSION.as_str())
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("list")
-                .about("List all available Minecraft versions")
-                .arg(
-                    arg!(-r --release "Only list release versions (default)")
-                        .action(ArgAction::SetTrue),
-                )
-                .arg(
-                    arg!(-p --"pre-release" "Only list pre-release versions")
-                        .action(ArgAction::SetTrue),
-                )
-                .arg(arg!(-s --snapshot "Only list snapshot versions").action(ArgAction::SetTrue))
-                .arg(arg!(-o --other "Only list other versions").action(ArgAction::SetTrue))
-                .arg(arg!(-a --all "List all versions").action(ArgAction::SetTrue))
-                .group(ArgGroup::new("filter").args([
-                    "release",
-                    "pre-release",
-                    "snapshot",
-                    "other",
-                    "all",
-                ])),
-        )
-        .subcommand(
-            Command::new("info")
-                .about("Get information about a Minecraft version")
-                .arg(
-                    arg!(-v --version <VERSION> "The version to get information about")
-                        .required(true)
-                        .value_parser(value_parser!(String)),
-                ),
-        )
-        .subcommand(
-            Command::new("install")
-                .about("Install a Minecraft version")
-                .after_help("Defaults to latest release version if none is provided")
-                .arg(
-                    arg!(-v --version "The version(s) to install")
-                        .action(ArgAction::Append)
-                        .value_delimiter(',') // splits as argv regardless
-                        .num_args(0..)
-                        .value_parser(value_parser!(String)),
-                ),
-        )
-        .subcommand(
-            Command::new("run")
-                .about("Run a Minecraft version")
-                .after_help("Version must be installed first")
-                .arg(
-                    arg!(-v --version <VERSION> "The version to run")
-                        .required(true)
-                        .value_parser(value_parser!(String)), // parse as String here, validate later
-                ),
-        )
-        .subcommand(
-            Command::new("locate")
-                .about("Print the path to a config file or instance directory")
-                .after_help("Supported locations:\n\
-                \tjre | java - The Java Runtime Environment directory\n\
-                \tinstances | versions | server - The directory containing Minecraft server versions\n\
-                \tconfig | settings - The directory containing config files")
-                .arg(arg!([what] "The file or directory to locate").required(true)),
-        );
-    // .subcommand(Command::new("uninstall").about("Uninstall a Minecraft version"))
+    let cli = Cli::parse();
+    dbg!(&cli);
 
-    let matches = cmd.get_matches_mut();
+    match cli.action {
+        Action::List { filter } => list_impl(filter).await?,
+        Action::Info { version } => info_impl(version).await?,
+        Action::Install { version } => install_impl(version).await?,
+        Action::Run { version } => run_impl(version).await?,
+        Action::Locate { what } => locate_impl(what)?,
+    }
 
-    // shared manifest between subcommands
-    let manifest_thread = tokio::spawn(async move {
-        let manifest = get_version_manifest().await?;
-        Ok::<_, eyre::Report>(manifest)
-    });
+    Ok(())
+}
 
-    if let Some(matches) = matches.subcommand_matches("list") {
-        let versions = manifest_thread.await??.versions;
-        let versions_filtered: Vec<&GameVersion> = if matches.get_flag("release") {
-            versions.iter().filter(|v| v.id.is_release()).collect_vec()
-        } else if matches.get_flag("pre-release") {
-            versions
-                .iter()
-                .filter(|v| v.id.is_pre_release())
-                .collect_vec()
-        } else if matches.get_flag("snapshot") {
-            versions.iter().filter(|v| v.id.is_snapshot()).collect_vec()
-        } else if matches.get_flag("other") {
-            versions.iter().filter(|v| v.id.is_other()).collect_vec()
-        } else if matches.get_flag("all") {
-            versions.iter().collect_vec()
-        } else {
-            versions.iter().filter(|v| v.id.is_release()).collect_vec()
-        };
+async fn list_impl(filter: Option<ListFilter>) -> Result<()> {
+    let filter = filter.unwrap_or_default();
 
-        if !std::io::stdout().is_terminal() {
-            versions_filtered.iter().for_each(|v| println!("{}", v.id));
-            return Ok(());
-        }
-
-        let term_width = match terminal_size::terminal_size() {
-            Some((terminal_size::Width(w), _)) => {
-                if w < 20 {
-                    cmd.error(ErrorKind::Io, "Terminal width is too small")
-                        .exit();
-                }
-                w as usize
+    let versions = MANIFEST
+        .get()
+        .await
+        .versions
+        .iter()
+        .filter(|v| {
+            match (
+                filter.release,
+                filter.pre_release,
+                filter.snapshot,
+                filter.other,
+                filter.all,
+            ) {
+                (true, _, _, _, _) => v.id.is_release(),
+                (_, true, _, _, _) => v.id.is_pre_release(),
+                (_, _, true, _, _) => v.id.is_snapshot(),
+                (_, _, _, true, _) => v.id.is_other(),
+                (_, _, _, _, true) => true,
+                _ => unreachable!(),
             }
-            _ => panic!("stdout is a terminal but has no size"),
-        };
+        })
+        .sorted()
+        .collect_vec();
 
-        // Print a terminal table with tabulated data
-        let max_len = versions_filtered
-            .iter()
-            .map(|v| v.id.to_string().len())
-            .max()
-            .expect("No versions found")
-            + 1;
+    if !std::io::stdout().is_terminal() {
+        versions.iter().for_each(|v| println!("{}", v.id));
+        return Ok(());
+    }
 
-        // unwrap checked above
-        for chunk in versions_filtered.chunks(term_width / (max_len + 1)) {
-            let row = chunk
-                .iter()
-                .map(|v| format!("{:max_len$}", v.id.to_string()))
-                .join(" ");
-            println!("{}", row.trim());
-        }
-    } else if let Some(matches) = matches.subcommand_matches("info") {
-        let manifest = manifest_thread.await??;
-        let versions = manifest.versions;
-        let version_ids = versions.iter().map(|v| &v.id).collect_vec();
+    let mut table = Table::new();
+    table.set_format(
+        FormatBuilder::new()
+            .column_separator(' ')
+            .borders(' ')
+            .padding(1, 1)
+            .build(),
+    );
+    table.set_titles(row![b => "Version", "Type", "Release Date"]);
+    for version in versions {
+        table.add_row(Row::new(vec![
+            Cell::new(&version.id.to_string()),
+            Cell::new(&version.release_type.to_string()).style_spec(
+                match version.release_type.as_str() {
+                    "release" => "FG",
+                    "snapshot" => "FY",
+                    "old_beta" => "FB",
+                    "old_alpha" => "FR",
+                    _ => unreachable!(),
+                },
+            ),
+            Cell::new(&version.release_time.to_string()),
+        ]));
+    }
+    table.printstd();
 
-        let version: VersionNumber = matches
-            .get_one::<String>("version")
-            .expect("No version provided")
-            .parse()
-            .expect("infallible");
+    Ok(())
+}
 
-        if !version_ids.contains(&&version) {
-            cmd.error(
-                ErrorKind::ValueValidation,
-                format!("Invalid version: {version}"),
-            )
-            .exit();
-        }
+async fn info_impl(version: VersionNumber) -> Result<()> {
+    let game_version = MANIFEST
+        .get()
+        .await
+        .versions
+        .iter()
+        .find(|v| v.id == version);
 
-        let version = versions
-            .iter()
-            .find(|v| v.id == version)
-            .expect("infallible"); // checked above
-
+    if let Some(version) = game_version {
         let time_format = "%-d %B %Y at %-I:%M:%S%P UTC";
         let message = format!(
             "Version {} ({})\nReleased: {}\nLast updated: {}",
@@ -183,80 +232,102 @@ async fn main() -> Result<()> {
         );
 
         println!("{message}");
-    } else if let Some(matches) = matches.subcommand_matches("install") {
-        let manifest = manifest_thread.await??;
-        let versions = manifest.versions;
-        let version_ids = versions.iter().map(|v| &v.id).collect_vec();
-        let latest = manifest.latest;
+    } else {
+        Cli::command()
+            .error(
+                ErrorKind::ValueValidation,
+                format!("No such version: {version}"),
+            )
+            .exit();
+    }
 
-        if let Some(matches) = matches.get_many::<String>("version") {
-            let (valid, invalid): (Vec<_>, Vec<_>) = matches
-                .into_iter()
-                .map(|v| v.parse::<VersionNumber>().expect("infallible"))
-                .partition(|v| version_ids.contains(&v));
+    Ok(())
+}
 
-            if valid.is_empty() {
-                cmd.error(
-                    ErrorKind::ValueValidation,
-                    format!("No valid versions found (got {})", invalid.len()),
-                )
-                .exit();
-            }
+async fn install_impl(version: Option<Vec<VersionNumber>>) -> Result<()> {
+    let manifest = MANIFEST.get().await;
+    let versions = &manifest.versions;
+    let version_ids = versions.iter().map(|v| &v.id).collect_vec();
+    let latest = &manifest.latest;
 
-            let mut message = format!(
-                "Installing {} version{}: {}",
-                valid.len(),
-                if valid.len() == 1 { "" } else { "s" },
-                valid.iter().map(ToString::to_string).join(", ")
-            );
-
-            if !invalid.is_empty() {
-                message.push_str(&format!(
-                    " (skipped {} invalid version{}: {})",
-                    invalid.len(),
-                    if invalid.len() == 1 { "" } else { "s" },
-                    invalid.iter().map(ToString::to_string).join(", ")
-                ));
-            }
-
-            println!("{message}\n");
-
-            let to_install_versions = versions
-                .iter()
-                .filter(|v| valid.contains(&v.id))
-                .collect_vec();
-
-            app::install_versions(to_install_versions)
-                .await
-                .wrap_err("Error while installing multiple versions")?;
-        } else {
-            println!("Installing latest release version\n");
-            let latest = versions
-                .iter()
-                .find(|v| v.id == latest.release)
-                .ok_or_else(|| eyre!("No latest release version found"))?;
-
-            app::install_versions(vec![latest])
-                .await
-                .wrap_err("Error while installing latest version")?;
-        }
-    } else if let Some(matches) = matches.subcommand_matches("run") {
-        let version: VersionNumber = matches
-            .get_one::<String>("version")
-            .expect("No version provided")
-            .parse()
-            .expect("infallible");
-
-        app::run_instance(version)
+    if version.is_none() {
+        println!("Installing latest release version\n");
+        let latest = versions
+            .iter()
+            .find(|v| v.id == latest.release)
+            .ok_or_else(|| eyre!("No latest release version found"))?;
+        app::install_versions(vec![latest])
             .await
-            .wrap_err("Error while running server")?;
-    } else if let Some(matches) = matches.subcommand_matches("locate") {
-        let what = matches
-            .get_one::<String>("what")
-            .expect("No input provided");
+            .wrap_err("Error while installing latest version")?;
 
-        app::locate(what).wrap_err(format!("Error while locating {what}"))?;
-    };
+        return Ok(());
+    }
+
+    let version = version.unwrap();
+    if version.is_empty() {
+        Cli::command()
+            .error(ErrorKind::ValueValidation, "No version provided")
+            .exit();
+    }
+
+    let (valid, invalid): (Vec<_>, Vec<_>) = version.iter().partition(|v| version_ids.contains(&v));
+
+    if valid.is_empty() {
+        Cli::command()
+            .error(
+                ErrorKind::ValueValidation,
+                format!("No valid versions found (got {})", invalid.len()),
+            )
+            .exit();
+    }
+
+    let mut message = format!(
+        "Installing {} version{}: {}",
+        valid.len(),
+        if valid.len() == 1 { "" } else { "s" },
+        valid.iter().map(ToString::to_string).join(", ")
+    );
+
+    if !invalid.is_empty() {
+        message.push_str(&format!(
+            " (skipped {} invalid version{}: {})",
+            invalid.len(),
+            if invalid.len() == 1 { "" } else { "s" },
+            invalid.iter().map(ToString::to_string).join(", ")
+        ));
+    }
+
+    println!("{message}\n");
+
+    let to_install_versions = versions
+        .iter()
+        .filter(|v| valid.contains(&&v.id))
+        .collect_vec();
+    app::install_versions(to_install_versions)
+        .await
+        .wrap_err("Error while installing versions")?;
+
+    Ok(())
+}
+
+async fn run_impl(version: String) -> Result<()> {
+    app::run_instance(version.parse()?)
+        .await
+        .wrap_err("Error while running server")?;
+
+    Ok(())
+}
+
+fn locate_impl(what: WhatEnum) -> Result<()> {
+    // TODO: pass directly
+    let what = match what {
+        WhatEnum::Java => "java",
+        WhatEnum::Instance => "instance",
+        WhatEnum::Config => "config",
+    }
+    .to_string();
+
+    app::locate(&what).wrap_err(format!("Error while locating `{what}`"))?;
 
     Ok(())
 }
