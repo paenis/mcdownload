30c848dfcbf4468b0ff4c203a6aaf89d885923ee
paenis <46578974+paenis@users.noreply.github.com>
2024-11-18 21:49:04 -0500

    initial commit

--- build.rs
+++
@@ -1,9 +0,0 @@
-use vergen::EmitBuilder;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    EmitBuilder::builder()
-        .git_sha(true)
-        .cargo_opt_level()
-        .emit()?;
-    Ok(())
-}
--- src/utils/mod.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) mod macros;
-pub(crate) mod net;
