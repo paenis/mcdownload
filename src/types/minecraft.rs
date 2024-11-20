c7bef28f5ed3cacc4a10708520ccbd0001127f43
paenis <46578974+paenis@users.noreply.github.com>
2024-11-20 01:50:35 -0500

    context, notes, tests, fmt

--- src/types/minecraft.rs
+++ src/types/minecraft.rs
@@ -1,9 +1,9 @@
 use std::str::FromStr;
 
 use derive_more::derive::Display;
-use winnow::ascii::{dec_uint, digit1};
-use winnow::combinator::{alt, opt, preceded};
-use winnow::error::{ContextError, StrContext};
+use winnow::ascii::digit1;
+use winnow::combinator::{alt, fail, opt, preceded};
+use winnow::error::{StrContext, StrContextValue};
 use winnow::prelude::*;
 use winnow::seq;
 use winnow::stream::AsChar;
@@ -37,12 +37,16 @@ impl FromStr for ReleaseVersionNumber {
 }
 
 fn release_version(i: &mut &str) -> PResult<ReleaseVersionNumber> {
-    let digits = dec_uint::<_, u8, ContextError>;
     let (major, minor, patch) = seq!(
-        digits.context(StrContext::Label("major")),
+        digit1.parse_to().context(StrContext::Label("major")),
         _: '.',
-        digits.context(StrContext::Label("minor")),
-        opt(preceded('.', digits.context(StrContext::Label("patch"))))
+        digit1.parse_to().context(StrContext::Label("minor")),
+        opt(preceded(
+            '.',
+            digit1.parse_to()
+                .context(StrContext::Label("patch"))
+                .context(StrContext::Expected(StrContextValue::Description("patch"))),
+        )),
     )
     .parse_next(i)?;
 
@@ -60,12 +64,21 @@ struct PreReleaseVersionNumber {
     pre_release: String,
 }
 
+impl FromStr for PreReleaseVersionNumber {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        pre_release_version.parse(s).map_err(|e| e.to_string())
+    }
+}
+
 fn pre_release_version(i: &mut &str) -> PResult<PreReleaseVersionNumber> {
     let (rv, pre_s, pre_n) = seq!(
-        release_version,
-        _: '-',
-        alt(("pre", "rc")),
-        digit1
+        release_version.context(StrContext::Label("release version")),
+        _: '-'.context(StrContext::Label("pre-release separator"))
+            .context(StrContext::Expected(StrContextValue::CharLiteral('-'))),
+        alt(("pre", "rc")).context(StrContext::Label("pre-release type")),
+        digit1.context(StrContext::Label("pre-release number")),
     )
     .parse_next(i)?;
 
@@ -91,10 +104,21 @@ impl FromStr for VersionNumber {
 }
 
 fn version_number(i: &mut &str) -> PResult<VersionNumber> {
+    // NOTE: winnow seems to be greedy/doesn't backtrack in some cases, so we need to try the most specific parsers first.
+    // in practice, this means we need to try pre-release before release (which it contains)
     alt((
-        release_version.map(VersionNumber::Release),
-        take_while(1.., (AsChar::is_alphanum, '.', '-', '_', ' '))
-            .map(|s: &str| VersionNumber::Other(s.into())),
+        pre_release_version
+            .map(VersionNumber::PreRelease)
+            .context(StrContext::Label("pre-release version")),
+        release_version
+            .map(VersionNumber::Release)
+            .context(StrContext::Label("release version")),
+        take_while(4.., (AsChar::is_alphanum, '.', '-', '_', ' '))
+            .map(|s: &str| VersionNumber::Other(s.into()))
+            .context(StrContext::Label("other version")),
+        fail.context(StrContext::Expected(StrContextValue::Description(
+            "version number",
+        ))),
     ))
     .parse_next(i)
 }
@@ -121,6 +145,7 @@ mod tests {
         };
     }
 
+    // NOTE: only useful for canonical representations of the version numbers, i.e. as found in the manifest
     macro_rules! test_bijective {
         ($name:ident: $parser:ident($input:expr)) => {
             #[test]
@@ -136,7 +161,7 @@ mod tests {
     test_parse!(parse_release3: release_version("1") => panic);
     test_parse!(parse_release4: release_version("1.2.") => panic);
     test_parse!(parse_release5: release_version("1.2.3.") => panic);
-    test_parse!(parse_release6: release_version("0.01.2") => panic);
+    test_parse!(parse_release6: release_version("0.01.2") => ReleaseVersionNumber { major: 0, minor: 1, patch: 2 });
     test_parse!(parse_release7: release_version("10.12.24") => ReleaseVersionNumber { major: 10, minor: 12, patch: 24 });
     test_parse!(parse_release8: release_version("1.0.256") => panic);
     test_bijective!(release_bijective: release_version("1.2.3"));
@@ -148,6 +173,7 @@ mod tests {
     test_bijective!(pre_release_bijective: pre_release_version("1.2.3-pre1"));
 
     test_parse!(parse_version1: version_number("1.2.3") => VersionNumber::Release(ReleaseVersionNumber { major: 1, minor: 2, patch: 3 }));
-    test_parse!(parse_version2: version_number("") => panic);
-    test_bijective!(version_bijective: version_number("foo"));
+    test_parse!(parse_version2: version_number("1.2.3-pre1") => VersionNumber::PreRelease(PreReleaseVersionNumber { release: ReleaseVersionNumber { major: 1, minor: 2, patch: 3 }, pre_release: _ }));
+    test_parse!(parse_version3: version_number("") => panic);
+    test_bijective!(version_bijective: version_number("foobar"));
 }
